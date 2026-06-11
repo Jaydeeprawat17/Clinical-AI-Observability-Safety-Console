f25d17ca0ec1898d5e2ee1e579547616b728bca5
Jaydeep Rawat <kr7102958@gmail.com>
2026-06-12 01:24:10 +0530

    chore: untrack .env and remove unused src and assets folders

--- src/utils/imageToBase64.ts
+++
@@ -1,8 +0,0 @@
-import * as FileSystem from 'expo-file-system/legacy';
-
-export const uriToBase64 = async (uri: string): Promise<string> => {
-  const base64 = await FileSystem.readAsStringAsync(uri, {
-    encoding: 'base64',
-  });
-  return base64;
-};
